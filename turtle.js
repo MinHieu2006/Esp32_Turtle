37bf60dd7f7833d22acfc5e06941ff265a03e881
Lương Minh Hiếu <50196971+MinHieu2006@users.noreply.github.com>
2024-11-13 23:06:25 +0700

    Update

---
+++ turtle.js
@@ -0,0 +1,23 @@
+
+//DOCUMENTATION: https://hanumanum.github.io/js-turtle/
+/*
+showGrid(20);      
+forward(distance)  
+right(angle)       
+left(angle) 	   
+goto(x,y) 	       
+clear() 	       
+penup() 	       
+pendown() 	       
+reset() 	       
+angle(angle)	   
+width(width)       
+
+color(r,g,b)
+color([r,g,b])
+color("red")
+color("#ff0000")
+*/
+
+
+go(100);
